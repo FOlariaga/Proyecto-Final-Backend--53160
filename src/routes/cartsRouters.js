1603bcdbaeecae065d510740f53887493fb40602
FOlariaga <olariagafederico2@gmail.com>
2024-04-24 19:25:03 -0300

    control de primera entrega

--- src/routes/cartsRouters.js
+++ src/routes/cartsRouters.js
@@ -7,21 +7,18 @@ const manager = new CartsManager(`${config.DIRNAME}/mocks/carts.json`)
 
 router.post("/", async (req, res) => {
     await manager.createCart()
-    console.log("api/carts funciona con post")
 })
 
 router.post("/:cid/product/:pid", async (req, res) => {
     const cid = +req.params.cid
     const pid = +req.params.pid
     await manager.addProductToCart(cid, pid)
-    console.log("api/carts/:cid funciona con post")
 })
 
 router.get("/:cid", async (req, res) => {
     const id = req.params.cid
     const productsCart = await manager.getCartById(id)
-    res.send({status: "funciona", payload: productsCart})
-    console.log("api/carts/:cid funciona con get")
+    res.send({status: "OK", payload: productsCart})
 })
 
 export default router
--- src/routes/productsRouters.js
+++ src/routes/productsRouters.js
@@ -7,32 +7,27 @@ const manager = new ProductManager(`${config.DIRNAME}/mocks/products.json`)
 router.get("/", async (req, res) => {
     const limit = req.query.limit
     const products =  await manager.getProduct(limit)
-    res.send({status:"funciona", payload: products})
-    console.log("api/products funciona con get")
+    res.send({status:"OK", payload: products})
 })
 
 router.get("/:pid", async (req, res) => {
     const id = req.params.pid
     const product = await manager.getProductById(id)
-    res.send({status:"funciona", payload: product})
-    console.log("api/products/:pid funciona con get")
+    res.send({status:"OK", payload: product})
 })
 
 router.post("/", async (req, res) => {
     await manager.addProduct(req.body)
-    console.log("api/products funciona con post")
 })
 
 router.put("/:pid", async (req, res) => {
     const id = req.params.pid
     await manager.updateProduct(id, req.body)
-    console.log("api/products/:pid funciona con put")
 })
 
 router.delete("/:pid", async (req, res) => {
     const id = req.params.pid
     await manager.deleteProduct(id)
-    console.log("api/products/:pid funciona con dete")
 })
 
 export default router
